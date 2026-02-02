7c9b32005fb1e0cae6185da2978ffd57d1e0658f
vartika126 <vartikasaini16@gmail.com>
2026-02-02 19:26:30 +0530

    squarepatternbasic

--- pattern.py/1stbasic  triangle.py
+++ pattern.py/1stbasic  triangle.py
@@ -31,15 +31,62 @@
 #         print(chr(65+j),end='')
 #     print()    
 
-n= 5
-temp=0
-for i in range(n):
-    for j in range(i+1):
-        print(chr(65+temp),end='')
-        temp=temp+1
-    print()    
+# n= 5
+# temp=0
+# for i in range(n):
+#     for j in range(i+1):
+#         print(chr(65+temp),end='')
+#         temp=temp+1
+#     print()    
 
+# qq.1
+# n=4
+# for i in range(n):
+#     for j in range(i+1):
+#         print(chr(65+j),end='')
+#     print()    
 
+# qq.2
+# n=4
+# for i in range(n):
+#         print(chr(68-i)*(i+1)) 
+
+# qq.3
+# n=4
+# for i in range(n):
+#     for j in range(i+1):
+#         print(chr(65+i),end='')
+#     print()
+
+# qq.4
+# n=4
+# for i in range(n):
+#     for j in range(i+1):
+#         print(chr(65+j),end='')
+#     print()   
+    
+
+#  qq.5
+# n=4
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print(chr(64+i),end='')
+#     print()       
+
+# qq.6
+# n=4
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print(chr(64+j),end='')
+#     print()   
+    
+# qq.7
+# n=5
+# for i in range(n):
+#     print(" "*(n-i)+'*'*i)
+#     print()
+
+  
 
 
 
