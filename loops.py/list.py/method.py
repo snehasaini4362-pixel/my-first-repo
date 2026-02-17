92afa3e4d23d03d164ba65c9b8376399afbed9d2
vartika126 <vartikasaini16@gmail.com>
2026-02-17 21:48:33 +0530

    added new file

---
+++ loops.py/list.py/method.py
@@ -0,0 +1,173 @@
+# qq.1 find the product of all element[1,2,3,4]
+# li=[1,2,3,4,]
+# prod=1
+# for i in li:
+#     prod=prod*i
+# print(prod)    
+
+# qq.2 find the largest element in the list[1,2,3,4]
+# li=[1,2,3,4]
+# print(max(li))
+
+# qq.3 insert,append,extend()
+# (1) li=[1,2,3,4]
+# li.insert(1,200)
+# print(li)
+
+# (2)
+# li=[1,2,3,4]
+# li.append(5)
+# print(li)
+
+# (3)
+# li=[1,2,3,4]
+# li.extend([5,6,7,8,9])
+# print(li)
+
+# qq.4 find the particular from the list=index
+# li=[1,2,3,4,5]
+# print(li[0],li[1])
+
+# qq.5 seprate out the even and odd elements from the list. even[2,4,6] odd=[1,3,5,7]
+
+# li=[1,2,3,4,5,6,7]
+# even=[]
+# odd=[]
+# for i in li:
+#     if i%2==0:
+#         even.append(i)
+#     else:
+#         odd.append(i)
+# print("even",even)
+# print("odd",odd)            
+
+# qq.6
+# li=[2,3,67,89,5]
+# print(li) 
+
+# qq.7
+# li=["python",5,5.7]
+# for i in li:
+#     print(i)
+
+# qq.8
+# li=[1,2,3,4]
+# print(len(li))
+
+# qq.9
+# li=[]
+# li.append(10)
+# li.append(20)
+# li.append(30)
+# print(li)
+
+# qq.10
+# li=[1,2,3,4,5,6]
+# print(li[0])
+
+# qq.11
+# li=[1,2,3,4,5,6]
+# print(li[-1])
+
+# qq.12
+# li=[1,2,3,4,5]
+# print(li[2])
+
+# qq.13
+# li=[1,2,3,4,5]
+# print(li[0:3])
+
+# qq.14
+# li=[1,2,3,4,5]
+# print(li[::-1])
+
+# qq.15
+# li=[1,2,3,4,5]
+# li[1]=100
+# print(li)
+
+# qq.16
+# li=[1,2,3,4]
+# li[-1]=100
+# print(li)
+
+# qq.17
+# li=[1,2,3,4,5]
+# li[1:4]=[10,20,30]
+# print(li)
+
+
+# qq.18
+# li=[1,2,3,4,5]
+# li.append(6)
+# print(li)
+
+# qq.19
+# li=[1,2,3,4,5]
+# li.extend([6,7,8,9])
+# print(li)
+
+# qq.20
+# li=[1,2,3,4,5]
+# li.insert(2,100)
+# print(li)
+
+# qq.21
+# append()=add a single element.
+# extend()=add a multiple element.
+
+# qq.22
+# li=[1,2,3,4]
+# li.remove(3)
+# print(li)
+
+# qq.23
+# li=[1,2,3,4,5]
+# li.clear()
+# print(li)
+
+# qq.24
+# li=[1,2,3,4,5]
+# for i in li:
+#     print(i)
+
+# qq.25
+# li=[1,2,3,4,5,6]
+# for i in li:
+#     if i%2==0:
+#         print(i)
+
+# qq.26
+# li=[5,12,50,20,3,6,40]
+# count=0
+# for i in li:
+#     if i > 10:
+#         count+=1
+# print(count)   
+
+# qq.27
+# li=[2,3,4,5]
+# sum=0
+# for i in li:
+#     sum+=i
+# print(sum)    
+
+# qq.28
+li=[10,5,50,30,20]
+max=li[0]
+for i in li:
+    if i > max:
+        max=i
+print(max)        
+
+
+
+
+
+
+
+
+
+
+
+
